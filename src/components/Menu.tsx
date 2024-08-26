1aea1bc0cbdfa6300a3e21b121f0ed3a1dfd6de5
muhazAhmed <muhazvla313@gmail.com>
2024-08-26 18:58:49 +0530

    fixed bugs from sidebar

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,21 +1,30 @@
 import Link from "next/link";
 import { menuItems } from "../lib/menuItems";
-import Image from "next/image";
+import { role } from "@/lib/data";
 
 const Menu = () => {
   return (
     <div className="mt-3 text-sm">
-      {menuItems.map((item: any, index: number) => (
-        <div className="flex flex-col gap-2" style={{paddingLeft: "10px"}} key={index}>
-          <Link
-            href={item?.href}
-            className="flex items-center justify-center lg:justify-start gap-4 py-2 text-white"
-          >
-            <i className={item?.icon} />
-            <span className="hidden lg:block">{item?.label}</span>
-          </Link>
-        </div>
-      ))}
+      {menuItems.map((item, index) => {
+        if (item.visible.includes(role)) {
+          return (
+            <div
+              className="flex flex-col gap-2"
+              style={{ paddingLeft: "10px" }}
+              key={index}
+            >
+              <Link
+                href={item.href}
+                className="flex items-center justify-center lg:justify-start gap-4 py-2 text-white rounded-md hover:bg-csSkyBlue hover:text-black md:px-2"
+              >
+                <i className={item.icon} />
+                <span className="hidden lg:block">{item.label}</span>
+              </Link>
+            </div>
+          );
+        }
+        return null;
+      })}
     </div>
   );
 };
